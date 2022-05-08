2a026a55808ca8b6b2438c3ee57d81ced8fe12bf
Piyush Garg <22243371+piyush888@users.noreply.github.com>
2022-05-08 14:57:23 -0700

    Removed retry part from Rewind and Unmatch API calls

--- Amore/Services/FirestoreServices.swift
+++ Amore/Services/FirestoreServices.swift
@@ -194,11 +194,6 @@ class FirestoreServices {
                             }
                             else if [400, 401, 403, 404, 500].contains(httpResponse.statusCode) {
                                 DispatchQueue.main.async {
-                                    if self.timeOutRetriesCount < 3 {
-                                        self.timeOutRetriesCount += 1
-                                        self.adminAuthModel.serverLogin()
-                                        self.storeLikesDislikes(apiToBeUsed: apiToBeUsed,onFailure: onFailure,onSuccess:onSuccess, swipedUserId: swipedUserId, swipeInfo: swipeInfo)
-                                    }
                                     self.requestInProcessing = false
                                 }
                             }
@@ -248,11 +243,6 @@ class FirestoreServices {
                         }
                         else if [400, 401, 403, 404, 500].contains(httpResponse.statusCode) {
                             DispatchQueue.main.async {
-                                if self.timeOutRetriesCount < 3 {
-                                    self.timeOutRetriesCount += 1
-                                    self.adminAuthModel.serverLogin()
-                                    self.unmatchUser(apiToBeUsed: apiToBeUsed, onFailure: onFailure, onSuccess: onSuccess, otherUserId: otherUserId)
-                                }
                                 self.requestInProcessing = false
                             }
                         }
