bb860c325369c29e91f9ea848a62e41ccad37110
Kshitiz Sharma <kshitizsharmav@gmail.com>
2022-11-19 15:45:24 -0500

    Mapping the Onboarding Work Field to the correct firestore work field: - Changed incorrect rewind count description from 2 to 3

--- Amore/Views/UserSettingsView/UserProfile.swift
+++ Amore/Views/UserSettingsView/UserProfile.swift
@@ -306,7 +306,7 @@ struct UserProfile: View {
                                                       subScriptText2:"5 Super likes everyday",
                                                       subScriptText3:"1 Boost a month",
                                                       subScriptText4:"2 Messages everyday",
-                                                      subScriptText5:"2 Rewinds everyday",
+                                                      subScriptText5:"3 Rewinds everyday",
                                                       showModal: $showModal,
                                                       geometry: geometry,
                                                       priceTabs: pricingData,
