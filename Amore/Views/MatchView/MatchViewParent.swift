5f1c1950badec0e5731633df4b7cfb40128cb681
Kshitiz Sharma <kshitizsharmav@gmail.com>
2023-04-14 07:57:22 -0400

    MatchView: To show a match between two users when the backend system pushes a match notification. Or We can also use chat service to trigger the match.

---
+++ Amore/Views/MatchView/MatchViewParent.swift
@@ -0,0 +1,20 @@
+//
+//  MatchViewParent.swift
+//  Amore
+//
+//  Created by Kshitiz Sharma on 4/14/23.
+//
+
+import SwiftUI
+
+struct MatchViewParent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchViewParent_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchViewParent()
+    }
+}
